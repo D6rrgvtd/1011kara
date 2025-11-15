1136bdb0c12d6fa62a742de0e1bc83f493bb4db5
ktc <>
2025-11-15 15:17:28 +0900

    aaa

---
+++ 1115/ソース2.cpp
@@ -0,0 +1,87 @@
+#include <d3d12.h>
+#include <dxgi1_4.h>
+
+class DIRECTX12
+{
+	IDXGIFactory4* CreateDXGIFactory()
+	{
+		IDXGIFactory4* factory;
+		UINT createFactoryFlags = 0;
+
+#if defined(_DEBUG)
+		createFactoryFlags = DXGI_CREATE_FACTORY_DEBUG;
+#endif // defined(_DEBUG)
+
+		HRESULT hr = CreateDXGIFactory2(createFactoryFlags, IID_PPV_ARGS(&factory));
+		if (FAILED(hr))
+		{
+			OutputDebugString("Failed to create DXGI Factory\n");
+			return nullptr;
+		}
+
+		return factory;
+	}
+
+	IDXGIAdapter* GetHardwareAdapter(IDXGIFactory4* factory)
+	{
+		IDXGIAdapter1* adapter;
+
+		for (UINT adapterIndex = 0; ; ++adapterIndex)
+		{
+			adapter = nullptr;
+			if (DXGI_ERROR_NOT_FOUND == factory->EnumAdapters1(adapterIndex, &adapter))
+			{
+				break;
+			}
+			DXGI_ADAPTER_DESC1 desc;
+			adapter->GetDesc1(&desc);
+
+			if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+			{
+				adapter->Release();
+				continue;
+			}
+
+			if (SUCCEEDED(D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_11_0, __uuidof(ID3D12Device), nullptr)))
+			{
+				return adapter;
+			}
+
+			adapter->Release();
+		}
+
+		return nullptr;
+
+	}
+
+	ID3D12Device* CreateD3D12Device(IDXGIAdapter1* adapter)
+	{
+		ID3D12Device* device;
+
+		HRESULT hr = D3D12CreateDevice
+		(
+			adapter,
+			D3D_FEATURE_LEVEL_11_0,
+			IID_PPV_ARGS(&device)
+		);
+		if (FAILED(hr))
+		{
+			hr = D3D12CreateDevice(
+				nullptr,
+				D3D_FEATURE_LEVEL_11_0,
+				IID_PPV_ARGS(&device)
+			);
+			if (FAILED(hr))
+			{
+				OutputDebugString("Failed to create D3D12 Device\n");
+				return nullptr;
+			}
+
+			OutputDebugString("Using software adapter (WARP)\n");
+		}
+
+		return device;
+	}
+
+
+};
